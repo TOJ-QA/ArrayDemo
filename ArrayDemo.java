1ec5a7bcc7b28454535d80a72769078feeea081d
QA Explorer <tracyojoseph@yahoo.com>
2026-07-14 20:48:44 -0400

    Add files via upload

---
+++ ArrayDemo.java
@@ -0,0 +1,27 @@
+package day6;
+
+public class ArrayDemo {
+
+	public static void main(String[] args) 
+	{
+		//int i=10;
+		//int j =20;
+		//int k =30;
+				
+			int i [] = {10, 20, 30, 40, 50};
+			//int  [] i = {10, 20, 30, 40, 50};
+			/*System.out.println(i[0]);
+			System.out.println(i[1]);
+			System.out.println(i[2]);
+			System.out.println(i[3]);
+			System.out.println(i[4]);*/
+			//System.out.println(i.length);
+				
+			for (int x=0; x<i.length; x++)
+				{
+					System.out.println(i[x]);
+				}
+		
+		}
+
+}
